f8ffc0fd42b17dd09c6d50240deee69c42346c4e
Chris <christorper@gmail.com>
2022-04-25 00:31:44 -0700

    Place ships horizontally on gameboard

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,10 +1,27 @@
-const gameboard = (board) => {
-  for (let i = 0; i < 10; ++i) {
-    board[String.fromCharCode(i + 65)] = [];
-    board[String.fromCharCode(i + 65)].length = 10;
-  }
+import { ship } from "./ships";
 
-  return { board };
+const gameboard = () => {
+  const makeBoard = () => {
+    const brd = {};
+    for (let i = 0; i < 10; ++i) {
+      brd[String.fromCharCode(i + 65)] = [];
+      brd[String.fromCharCode(i + 65)].length = 10;
+    }
+    return brd;
+  };
+
+  const board = makeBoard();
+
+  const placeShip = (shipType, direction, startRow, startColumn) => {
+    if (direction === "horizontal") {
+      for (let i = startColumn - 1; i < startColumn - 1 + shipType.length; ++i) {
+        board[startRow][i] = "o";
+      }
+    }
+    return board;
+  };
+
+  return { board, placeShip };
 };
 
 export { gameboard };
--- tests/gameboard.test.js
+++ tests/gameboard.test.js
@@ -1,11 +1,33 @@
 import { gameboard } from "../src/gameboard";
+import { ship } from "../src/ships";
 
 test("Gameboard returns a 10 x 10 grid", () => {
-  expect(gameboard({}).board).toEqual({
+  expect(gameboard().board).toEqual({
     A: [, , , , , , , , , ,],
     B: [, , , , , , , , , ,],
     C: [, , , , , , , , , ,],
     D: [, , , , , , , , , ,],
+    E: [, , , , , , , , , ,],
+    F: [, , , , , , , , , ,],
+    G: [, , , , , , , , , ,],
+    H: [, , , , , , , , , ,],
+    I: [, , , , , , , , , ,],
+    J: [, , , , , , , , , ,],
+  });
+});
+
+test("placeShip places a ship horizontally", () => {
+  const placeShipMock = jest.fn(() =>
+    gameboard().placeShip(ship(5), "horizontal", "D", 3)
+  );
+
+  placeShipMock();
+
+  expect(placeShipMock).toHaveReturnedWith({
+    A: [, , , , , , , , , ,],
+    B: [, , , , , , , , , ,],
+    C: [, , , , , , , , , ,],
+    D: [, , "o", "o", "o", "o", "o", , , ,],
     E: [, , , , , , , , , ,],
     F: [, , , , , , , , , ,],
     G: [, , , , , , , , , ,],
